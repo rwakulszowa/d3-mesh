c36944b1f54885a0703c5c8c79f9509a8aa1708f
rwakulszowa <zajactomasz1@yahoo.com>
2016-06-09 11:58:24 +0200

    Mesh is now usable, added simple example

--- src/mesh.js
+++ src/mesh.js
@@ -2,38 +2,45 @@ export default function() {
   var dims,
       divs;
 
-  //TODO: cell.js
+  //TODO: cell.js, node.js?
   //TODO: 3 results: interpolate(relX, relY, ...) -> array of interpolated dims, mesh(dims) -> N-D array of cells / callable?, cover(context, data) -> selection of {data, d1, d2, dx...}
   //TODO: provide some useful interpolators out of the box
 
   function mesh(indices) {
-      var ans = [];
-      for (var i in dims) {
-        ans[i] = dims[i](indices[i] / divs[i]);
+    return indices.map(
+      function(el, i) {
+        // map each index to a pair (index, index+1) scaled to range [0, 1]
+        return [ +el / divs[i], ( +el + 1) / divs[i] ];
       }
-      return ans;
+    ).reduce(
+      function(obj, val, i) {
+        // convert to an object {d0: [start, end], d1:[start, end], dn...}
+        obj["d" + i] = val.map(function(el) { return dims[i](el); });
+        return obj;
+      }, {}
+    );
   }
 
   function interpolate(args) {
-      var ans = new Array(dims.length);
-      for (var i in args) {
-          ans[i] = dims[i](args[i]);
-      }
-      return ans;
+    return args.map(function(el, i) {
+      return Array.isArray(el) ? (
+        el.map(function(e) { return dims[i](e); })
+      ) : dims[i](el);
+    });
   }
 
   mesh.dims = function(_) {
-      return arguments.length ? (
-        dims = _,
-        mesh
-      ) : dims;
+    return arguments.length ? (
+      dims = _,
+      mesh
+    ) : dims;
   };
 
   mesh.divs = function(_) {
-      return arguments.length ? (
-        divs = _,
-        mesh
-      ) : divs;
+    return arguments.length ? (
+      divs = _,
+      mesh
+    ) : divs;
   };
 
   mesh.interpolate = interpolate;
