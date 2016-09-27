e9df84de1638e407d779c5224c8ce5571a9d1805
rwakulszowa <rwakulszowa1@gmail.com>
2016-09-27 22:57:09 +0200

    Bump version, update metadata

--- examples/d3-mesh.js
+++ examples/d3-mesh.js
@@ -2,238 +2,240 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (factory((global.d3_mesh = global.d3_mesh || {})));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
-  // Public - set create a new Cell
-  //
-  // nodes - array of start- and endpoints for each dimension
-  // data - value to be bound to a cell
-  // dims - list of dimension names (default: "xyz")
-  //
-  // Returns dimension or mesh
-  function Cell(nodes, data, dims) {
-    dims = dims || "xyz";
+// Public - set create a new Cell
+//
+// nodes - array of start- and endpoints for each dimension
+// data - value to be bound to a cell
+// dims - list of dimension names (default: "xyz")
+//
+// Returns dimension or mesh
+function Cell(nodes, data, dims) {
+  dims = dims || "xyz";
 
-    for (var i in nodes) {
-      this[dims[i]] = nodes[i];
-    }
-
-    this['data'] = data;
+  for (var i in nodes) {
+    this[dims[i]] = nodes[i];
   }
 
-  Cell.prototype = {
-    constructor: Cell
-  };
+  this['data'] = data;
+}
 
-  function dim() {
-    var domain = [0, 1],
-        shape = function(x) { return 1; };
+Cell.prototype = {
+  constructor: Cell
+};
 
-    // Public - divide dimension into divs elements
-    //
-    // divs - dimension divisions
-    //
-    // Returns an array of boundary points
-    function dimension(divs) {
-      return nodesToRanges(
-        coverDomain(
-          shapeToNodes(
-            makeNodes(divs)
-          )
+function dim() {
+  var domain = [0, 1],
+      shape = function(x) { return 1; };
+
+  // Public - divide dimension into divs elements
+  //
+  // divs - dimension divisions
+  //
+  // Returns an array of boundary points
+  function dimension(divs) {
+    return nodesToRanges(
+      coverDomain(
+        shapeToNodes(
+          makeNodes(divs)
         )
       )
-    }
-
-    // Private - convert shape variable to an array
-    //
-    // len - desired length
-    //
-    // Returns an array of len numbers
-    function makeNodes(len) {
-      if (Array.isArray(shape)) {
-        return expand(shape, len);
-      } else if (typeof shape == "function") {
-        return Array.apply(null, Array(len)).map(shape);
-      } else {
-        throw("shape must be an Array or a function")
-      }
-    }
-
-    // Private - expand the shape to len length
-    //
-    // blueprint - an array to expand
-    // len - desired length
-    //
-    // Returns a len-element array of numbers
-    function expand(blueprint, len) {
-      if (blueprint.length == 0) {
-        throw("empty blueprint");
-      }
-
-      function repeat(res) {
-        if (res.length == len) {
-          return res
-        } else {
-          return repeat(res.concat(blueprint.slice(0, len - res.length)));
-        }
-      }
-
-      return repeat([]);
-    }
-
-    // Private - convert shape to nodes (compute prefix sum)
-    //
-    // shape - an array of widths of each division
-    //
-    // Returns an array of shape.length + 1 nodes
-    function shapeToNodes(shape) {
-
-      function nextNode(nodes, shape) {
-        if (shape.length == 0) {
-          return nodes;
-        } else {
-          return nextNode(push(nodes, last(nodes) + shape.shift()), shape)
-        }
-      }
-
-      function push(arr, el) {  //TODO: use concat instead
-        arr.push(el);
-        return arr;
-      }
-
-      function last(arr) {
-        return arr[arr.length - 1];
-      }
-
-      return nextNode([0], shape);
-    }
-
-    // Private - transform nodes to cover domain
-    //
-    // nodes - an array of nodes
-    //
-    // Returns an array of nodes.length nodes spanning over domain
-    function coverDomain(nodes) {
-      var domainLength = domain[1] - domain[0],
-          min = Math.min.apply(Number.MAX_VALUE, nodes),
-          max = Math.max.apply(null, nodes),
-          diff = max - min;
-
-      return nodes.map(function(n) {
-        return domain[0] + domainLength * (n - min) / diff;
-      })
-    }
-
-    // Private - convert nodes to ranges
-    //
-    // nodes - an array of nodes
-    //
-    // Returns an array of nodes.length +1 objects {'a':..., 'b': ...}
-    function nodesToRanges(nodes) {
-
-      function build(nodes, ranges) {
-        if (nodes.length <= 1) {
-          return ranges;
-        } else {
-          return build(
-            nodes.slice(1, nodes.length),
-            ranges.concat({ 'a': nodes[0], 'b': nodes[1] })
-          )
-        }
-      }
-
-      return build(nodes, []);
-    }
-
-    // Public - set or get domain attribute
-    //
-    // _ - new domain value (optional)
-    //
-    // Returns domain or dimension
-    dimension.domain = function(_) {
-      return arguments.length ? (
-        domain = _,
-        dimension
-      ) : domain;
-    };
-
-    // Public - set or get shape attribute
-    //
-    // _ - new shape value (optional)
-    //
-    // Returns shape or dimension
-    dimension.shape = function(_) {
-      return arguments.length ? (
-        shape = _,
-        dimension
-      ) : shape;
-    };
-
-    return dimension;
+    )
   }
 
-  function mesh() {
-    var x = dim(),
-        y = dim();
+  // Private - convert shape variable to an array
+  //
+  // len - desired length
+  //
+  // Returns an array of len numbers
+  function makeNodes(len) {
+    if (Array.isArray(shape)) {
+      return expand(shape, len);
+    } else if (typeof shape == "function") {
+      return Array.apply(null, Array(len)).map(shape);
+    } else {
+      throw("shape must be an Array or a function")
+    }
+  }
 
-    //TODO: utils for irregular mesh (merged cells) - empty array cells? - map omits them
-    //NOTE: dimension animations can be handled by moving / passing / shuffling data
-    //TODO: allow other forms of digging - flatten cells, wrap all rows in Cell, swap x/y...
-
-    // Public - create a mesh bound with data
-    //
-    // data - 2D array of data to be bound
-    //
-    // Returns a 2D array of Cells
-    function mesh(data) {
-
-      function dig(data, dims, nodes) {
-        if (dims.length == 0) {
-          return new Cell(nodes, data);
-        } else {
-          var nodes_ = dims[0](data.length);
-          return data.map(function(d, i) {
-            return dig(
-              d,
-              dims.slice(1, dims.length),
-              nodes.concat(nodes_[i])
-            );
-          })
-        }
-      }
-
-      return dig(data, [x, y], []);
+  // Private - expand the shape to len length
+  //
+  // blueprint - an array to expand
+  // len - desired length
+  //
+  // Returns a len-element array of numbers
+  function expand(blueprint, len) {
+    if (blueprint.length == 0) {
+      throw("empty blueprint");
     }
 
-    // Public - set or get x attribute
-    //
-    // _ - new x value (optional)
-    //
-    // Returns dimension or mesh
-    mesh.x = function(_) {
-      return arguments.length ? (
-        x = _,
-        mesh
-      ) : x;
-    };
+    function repeat(res) {
+      if (res.length == len) {
+        return res
+      } else {
+        return repeat(res.concat(blueprint.slice(0, len - res.length)));
+      }
+    }
 
-    // Public - set or get y attribute
-    //
-    // _ - new y value (optional)
-    //
-    // Returns dimension or mesh
-    mesh.y = function(_) {
-      return arguments.length ? (
-        y = _,
-        mesh
-      ) : y;
-    };
+    return repeat([]);
+  }
 
-    return mesh;
+  // Private - convert shape to nodes (compute prefix sum)
+  //
+  // shape - an array of widths of each division
+  //
+  // Returns an array of shape.length + 1 nodes
+  function shapeToNodes(shape) {
+
+    function nextNode(nodes, shape) {
+      if (shape.length == 0) {
+        return nodes;
+      } else {
+        return nextNode(push(nodes, last(nodes) + shape.shift()), shape)
+      }
+    }
+
+    function push(arr, el) {  //TODO: use concat instead
+      arr.push(el);
+      return arr;
+    }
+
+    function last(arr) {
+      return arr[arr.length - 1];
+    }
+
+    return nextNode([0], shape);
+  }
+
+  // Private - transform nodes to cover domain
+  //
+  // nodes - an array of nodes
+  //
+  // Returns an array of nodes.length nodes spanning over domain
+  function coverDomain(nodes) {
+    var domainLength = domain[1] - domain[0],
+        min = Math.min.apply(Number.MAX_VALUE, nodes),
+        max = Math.max.apply(null, nodes),
+        diff = max - min;
+
+    return nodes.map(function(n) {
+      return domain[0] + domainLength * (n - min) / diff;
+    })
+  }
+
+  // Private - convert nodes to ranges
+  //
+  // nodes - an array of nodes
+  //
+  // Returns an array of nodes.length +1 objects {'a':..., 'b': ...}
+  function nodesToRanges(nodes) {
+
+    function build(nodes, ranges) {
+      if (nodes.length <= 1) {
+        return ranges;
+      } else {
+        return build(
+          nodes.slice(1, nodes.length),
+          ranges.concat({ 'a': nodes[0], 'b': nodes[1] })
+        )
+      }
+    }
+
+    return build(nodes, []);
+  }
+
+  // Public - set or get domain attribute
+  //
+  // _ - new domain value (optional)
+  //
+  // Returns domain or dimension
+  dimension.domain = function(_) {
+    return arguments.length ? (
+      domain = _,
+      dimension
+    ) : domain;
   };
 
-  exports.mesh = mesh;
-  exports.dimension = dim;
-  exports.Cell = Cell;
+  // Public - set or get shape attribute
+  //
+  // _ - new shape value (optional)
+  //
+  // Returns shape or dimension
+  dimension.shape = function(_) {
+    return arguments.length ? (
+      shape = _,
+      dimension
+    ) : shape;
+  };
 
-}));
+  return dimension;
+}
+
+function mesh() {
+  var x = dim(),
+      y = dim();
+
+  //TODO: utils for irregular mesh (merged cells) - empty array cells? - map omits them
+  //NOTE: dimension animations can be handled by moving / passing / shuffling data
+  //TODO: allow other forms of digging - flatten cells, wrap all rows in Cell, swap x/y...
+
+  // Public - create a mesh bound with data
+  //
+  // data - 2D array of data to be bound
+  //
+  // Returns a 2D array of Cells
+  function mesh(data) {
+
+    function dig(data, dims, nodes) {
+      if (dims.length == 0) {
+        return new Cell(nodes, data);
+      } else {
+        var nodes_ = dims[0](data.length);
+        return data.map(function(d, i) {
+          return dig(
+            d,
+            dims.slice(1, dims.length),
+            nodes.concat(nodes_[i])
+          );
+        })
+      }
+    }
+
+    return dig(data, [x, y], []);
+  }
+
+  // Public - set or get x attribute
+  //
+  // _ - new x value (optional)
+  //
+  // Returns dimension or mesh
+  mesh.x = function(_) {
+    return arguments.length ? (
+      x = _,
+      mesh
+    ) : x;
+  };
+
+  // Public - set or get y attribute
+  //
+  // _ - new y value (optional)
+  //
+  // Returns dimension or mesh
+  mesh.y = function(_) {
+    return arguments.length ? (
+      y = _,
+      mesh
+    ) : y;
+  };
+
+  return mesh;
+};
+
+exports.mesh = mesh;
+exports.dimension = dim;
+exports.Cell = Cell;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
